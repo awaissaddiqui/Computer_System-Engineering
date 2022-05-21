0cabc8abce39e537e79c779890342b713eb1953e
awais <awaissaddiqui143@gmail.com>
2022-05-21 17:56:44 +0500

    CP lab-6 Q.No= 1&2

--- lab-6-Q-1.cpp
+++ lab-6-Q-1.cpp
@@ -1,4 +1,11 @@
-#include <iostream>
+/* Q=1,Write a program that takes marks and 
+registration number as input and then displays 
+your name, grade and registration number using 
+function. (You must use your own name and registration number
+ also the grade must be calculated on the bases of marks).*/
+ /////////////////////////////////////////////////////////
+
+/*#include <iostream>
 using namespace std;
 string root(int);
  main(){
@@ -33,5 +40,51 @@ string root(int);
     		grade="Invalid Marks";
 		}
     return grade;
-   }
+   }   */
+   
+//////////////////////////////////////////////////////////////
 
+/* Q=2,Write a function minmax () 
+that takes four integers as input and display 
+the minimum and maximum number.*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void minmax(int,int,int,int);
+main(){
+	int value[3];
+   cout<<"Enter Four Numbers \n";
+   cin>>value[0]>>value[1]>>value[2]>>value[3];
+	minmax(value[0], value[1], value[2], value[3]);		
+}
+
+void minmax(int a, int b, int c, int d ){
+	int max,min;
+	if(a>b && a>c && a>d){
+		max=a;
+	}
+	else if(b>a && b>c && b>d){
+		max=b;
+	}
+		else if(c>a && c>b  && c>d){
+		max=c;
+	}
+		else if(d>a, d>b, d>c){
+		max=d;
+	}else{cout<<"Invalid";}
+	
+	if(a<b && a<c && a<d){
+		min=a;
+	}
+	else if(a>b && b<c && b<d){
+		min=b;
+	}
+	else if(c<b && a>c && c<d){
+		min=c;
+	}
+	else if(d<a, d<b, d<c){
+		min=d;
+	}else{cout<<"Invalid";}
+	cout<<"Maximum: "<<max;
+	cout<<"\nMinimum: "<<min;
+}
